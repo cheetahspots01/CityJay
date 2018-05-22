4b95e4e237dfa312b47e9e7c47b7965509fd5ddb
Tommy Gaessler <tommy.gaessler@gmail.com>
2018-05-22 17:45:18 -0600

    added touch event

--- main.js
+++ main.js
@@ -23,6 +23,8 @@ var mainState = {
       Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);
 
+    game.input.onDown.add(this.jump, this);
+
     this.pipes = game.add.group();
 
     this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
